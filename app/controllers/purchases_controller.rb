87fe5c03be9cc2c4a1ffa33d249a116377340b96
Billions Joel <joelatugonza@gmail.com>
2022-05-27 13:21:36 +0300

    Add new method

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -3,4 +3,9 @@ class PurchasesController < ApplicationController
     @purchases = Purchase.where(author: current_user, category_id: params[:category_id]).order(updated_at: :desc)
     @total = @purchases.sum(:amount)
   end
+
+  def new
+    @purchase = Purchase.new
+    set_users_categories
+  end
 end
