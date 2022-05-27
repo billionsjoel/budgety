5e487460a402567b11ec2f6eeab57f73c6cf6df9
Billions Joel <joelatugonza@gmail.com>
2022-05-27 17:22:54 +0300

    Add before actions

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Categories", type: :request do
+  let(:test_user) { create :user }
+  before { sign_in test_user }
+  let(:valid_category) { build :category, user: test_user }
+  let(:invalid_category) { build :category, name: nil }
+  let(:valid_attributes) { valid_category.attributes }
+
+  let(:invalid_attributes) { invalid_category.attributes }
+
   describe "GET /categories" do
     it "works! (now write some real specs)" do
       get categories_index_path
