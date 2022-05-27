4821cca41f403ca176b6254fc03e990d90292246
Billions Joel <joelatugonza@gmail.com>
2022-05-27 18:00:04 +0300

    Before do

--- spec/system/categories/index.html.erb_spec.rb
+++ spec/system/categories/index.html.erb_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 RSpec.describe 'categories/index', type: :system do
   let(:test_user) { create :user }
 
+  before do
+    driven_by(:rack_test)
+    sign_in test_user
+    visit categories_url
+  end
+
+
+
 end
