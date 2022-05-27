93da6e09214e34e8fdd8b54d7f912f801c0c62fe
Billions Joel <joelatugonza@gmail.com>
2022-05-27 13:21:10 +0300

    Add index method

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,6 @@
 class PurchasesController < ApplicationController
+  def index
+    @purchases = Purchase.where(author: current_user, category_id: params[:category_id]).order(updated_at: :desc)
+    @total = @purchases.sum(:amount)
+  end
 end
