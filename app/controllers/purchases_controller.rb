b774cb0483afb3fc7890e9ef13ad2eab6cb48f34
Billions Joel <joelatugonza@gmail.com>
2022-05-27 13:22:05 +0300

    Add create method

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -8,4 +8,22 @@ class PurchasesController < ApplicationController
     @purchase = Purchase.new
     set_users_categories
   end
+
+  def create
+    @purchase = Purchase.new(purchase_params)
+    @purchase.author = current_user
+    respond_to do |format|
+      if @purchase.save
+        format.html do
+          redirect_to category_purchases_url(category_id: @purchase.category_id),
+                      notice: 'Purchase was successfully created.'
+        end
+        format.json { render :show, status: :created, location: @purchase }
+      else
+        set_users_categories
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @purchase.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
