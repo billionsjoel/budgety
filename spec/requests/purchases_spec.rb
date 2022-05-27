ce9f6fc552e532cab6c21fc7c314111a94cb5d19
Billions Joel <joelatugonza@gmail.com>
2022-05-27 17:51:19 +0300

    Create test user info

--- spec/requests/purchases_spec.rb
+++ spec/requests/purchases_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Purchases", type: :request do
+  let(:test_user) { create :user }
+  let(:test_category) { create :category }
+  before { sign_in test_user }
+  let(:valid_purchase) { build :purchase, author: test_user, category: test_category }
+  let(:invalid_purchase) { build :purchase, name: nil, author: test_user, category: test_category }
+  let(:valid_attributes) { valid_purchase.attributes }
+
+  let(:invalid_attributes) { invalid_purchase.attributes }
   describe "GET /purchases" do
 
   end
