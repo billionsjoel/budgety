8979b33aa26ba5ac8dd0ac2ded3451372a86961f
Billions Joel <joelatugonza@gmail.com>
2022-05-27 12:31:58 +0300

    Add new method to create category and return response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,18 @@ class UsersController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user_id = current_user.id
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_url, notice: 'Category was successfully created.' }
+        format.json { render :show, status: :created, location: @category }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
