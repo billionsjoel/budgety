d6a6103026cd118c6224179dbf5ede480a148518
Billions Joel <joelatugonza@gmail.com>
2022-05-27 13:15:19 +0300

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_093609) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_101303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_093609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.float "amount"
+    t.bigint "category_id", null: false
+    t.bigint "author_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_purchases_on_author_id"
+    t.index ["category_id"], name: "index_purchases_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_093609) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "purchases", "categories"
+  add_foreign_key "purchases", "users", column: "author_id"
 end
