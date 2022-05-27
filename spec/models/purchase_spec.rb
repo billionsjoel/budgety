c4f38e5d4f2d542de5d78e91c1e12d2275e11f39
Billions Joel <joelatugonza@gmail.com>
2022-05-27 17:18:52 +0300

    Add tests for purchases

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :author }
+  it { should belong_to :category }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :amount }
 end
