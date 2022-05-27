26a5cae624bb8187506bf5f4feb2b1aff56de12d
Billions Joel <joelatugonza@gmail.com>
2022-05-27 19:19:53 +0300

    Add category and sign in user then visit link

--- spec/system/purchases/new.html.erb_spec.rb
+++ spec/system/purchases/new.html.erb_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe 'purchases/new', type: :system do
   let(:test_user) { create :user }
   let(:test_category) { create :category }
 
+  before do
+    driven_by(:rack_test)
+    sign_in test_user
+    visit new_category_purchase_url(test_category)
+  end
+
 end
