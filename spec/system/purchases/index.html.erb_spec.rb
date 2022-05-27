4c1186b5a3a2f5f98e17b63dfb3b0d57bf57b5e7
Billions Joel <joelatugonza@gmail.com>
2022-05-27 18:41:44 +0300

    Sign in user and visit link

--- spec/system/purchases/index.html.erb_spec.rb
+++ spec/system/purchases/index.html.erb_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe 'purchases/index', type: :system do
   let(:test_user) { create :user }
   let(:test_category) { create :category }
 
+  before do
+    driven_by(:rack_test)
+    sign_in test_user
+    visit category_purchases_url(test_category)
+  end
 end
