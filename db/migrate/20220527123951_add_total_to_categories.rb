a388fa18b14389d98862239ef5d52b876abf147a
Billions Joel <joelatugonza@gmail.com>
2022-05-27 15:40:55 +0300

    Add total to Cat

--- db/migrate/20220527123951_add_total_to_categories.rb
+++ db/migrate/20220527123951_add_total_to_categories.rb
@@ -1,4 +1,9 @@
 class AddTotalToCategories < ActiveRecord::Migration[7.0]
-  def change
+  def self.up
+    add_column :categories, :total, :integer, null: false, default: 0
+  end
+
+  def self.down
+    remove_column :categories, :total
   end
 end
