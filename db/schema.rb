74915b46d1d610c8d8f8660231838a6d5718c4d0
Billions Joel <joelatugonza@gmail.com>
2022-05-27 15:42:19 +0300

    Fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_27_123951) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_27_124146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_123951) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "total", default: 0, null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
