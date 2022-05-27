a281ae28d9774206dd7f7b4a53f7f8f946f9df5a
Billions Joel <joelatugonza@gmail.com>
2022-05-27 17:17:14 +0300

    Add factory

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :category do
-    
+    name { 'MyString' }
+    icon { 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80' }
+    user
   end
 end
