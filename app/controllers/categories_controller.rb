642943791299e306a1325d383f626b1d0aa028a1
Billions Joel <joelatugonza@gmail.com>
2022-05-27 13:20:44 +0300

    Add before action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
  before_action :authenticate_user!
+ before_action :set_category, only: %i[show edit update destroy]
 
   def index
     categories = Category.includes(:purchases).all
