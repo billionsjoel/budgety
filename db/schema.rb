1fdeb88712ae3c56a5ebbbf637100ff014d69cd4
Billions Joel <joelatugonza@gmail.com>
2022-05-27 14:07:53 +0300

    Fix device migration

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_27_110058) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
