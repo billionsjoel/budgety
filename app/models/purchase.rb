cfd730098c48d58397dfae69b730952e6002a22a
Billions Joel <joelatugonza@gmail.com>
2022-05-27 13:15:02 +0300

    Add purchase migration

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,6 @@
 class Purchase < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
-
   validates :amount, presence: true
   validates :name, presence: true
 end
--- db/migrate/20220527101303_create_purchases.rb
+++ db/migrate/20220527101303_create_purchases.rb
@@ -1,7 +1,10 @@
 class CreatePurchases < ActiveRecord::Migration[7.0]
   def change
     create_table :purchases do |t|
-
+      t.float :amount
+      t.references :category, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: {to_table: :users}
+      t.string :name
       t.timestamps
     end
   end
