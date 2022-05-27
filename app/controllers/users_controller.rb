ea104070db632d8bb443139a04699cf06f69768b
Billions Joel <joelatugonza@gmail.com>
2022-05-27 12:30:32 +0300

    Add index method to query all categories using n+1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    categories = Category.includes(:purchases).all
+    @categories = categories.map do |category|
+      category.total = category.purchases.where(author: current_user).sum(:amount)
+      category
+    end
+  end
 end
