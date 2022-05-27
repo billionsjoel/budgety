09b9dc92dda88b08663664b59b59b9cd19e30a95
Billions Joel <joelatugonza@gmail.com>
2022-05-27 12:11:38 +0300

    Add resource routes for categories and purchases

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: "splash#index"
 
+  resources :splash, only: [:index]
+  resources :categories do
+    resources :purchases
+  end
+
   resources :users
 end
