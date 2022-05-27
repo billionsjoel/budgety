803c80b9866a9e0b7c16d416e4f2035e67a65601
Billions Joel <joelatugonza@gmail.com>
2022-05-27 18:07:54 +0300

    Sign in user and visit url

--- spec/system/categories/new.html.erb_spce.rb
+++ spec/system/categories/new.html.erb_spce.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'categories/new', type: :system do
   let(:test_user) { create :user }
+
+  before do
+    driven_by(:rack_test)
+    sign_in test_user
+    visit new_category_url
+  end
+
 end
